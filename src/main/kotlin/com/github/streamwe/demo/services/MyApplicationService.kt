b5de403838d4c1a7ebd610e638293dab5018c872
GitHub Action <action@github.com>
2021-04-15 08:53:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/streamwe/demo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/streamwe/demo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.streamwe.demo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.streamwe.demo.MyBundle
 
 class MyApplicationService {
 
